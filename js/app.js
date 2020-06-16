defafa450106a7bdc9960c8ac3c03b43913ab930
Camila <caming2309@gmail.com>
2020-06-16 02:32:24 -0300

    WORKING

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-let image = document.querySelector('.image');
+let imageMovie = document.querySelector('.image');
 let title = document.querySelector('.movie-title')
 let button = document.querySelector('.button')
 
@@ -9,18 +9,19 @@ let movies = [
     },
     {
         title: 'Doggo2',
-        image: 'https://https://i.imgur.com/z9UAzcd.jpg.dreamstime.com/z/perrito-lindo-de-la-mezclado-raza-del-golden-retriever-33418956.jpg',
+        image: 'https://estaticos.muyinteresante.es/media/cache/1140x_thumb/uploads/images/gallery/59c4f5655bafe82c692a7052/gato-marron_0.jpg',
     }
 ]
 
-let randomMovie = movies[Math.floor(Math.random()*movies.length)].title;
 
 
 button.addEventListener('click', function () {
-    console.log(randomMovie);
-    // image.style.backgroundImage = "url('https://thumbs.dreamstime.com/z/perrito-lindo-de-la-mezclado-raza-del-golden-retriever-33418956.jpg')";
-    // image.style.backgroundSize = 'cover';
-    // image.style.backgroundPosition = 'left';
+    let randomMovieTitle = movies[Math.floor(Math.random()*movies.length)].title;    
+    let randomMovie = movies[Math.floor(Math.random()*movies.length)];   
 
+    imageMovie.style.backgroundImage = `url('${randomMovie.image}')`
+    imageMovie.style.backgroundSize = 'cover';
+
+    title.innerText = randomMovie.title;
 })
 
