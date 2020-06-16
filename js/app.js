da0b53f8c27899f558a206cdbabe0fcc6880f5bf
Camila <caming2309@gmail.com>
2020-06-16 01:37:10 -0300

    random picker

--- js/app.js
+++ js/app.js
@@ -2,23 +2,25 @@ let image = document.querySelector('.image');
 let title = document.querySelector('.movie-title')
 let button = document.querySelector('.button')
 
-const movies = [
+let movies = [
     {
         title: 'Doggo1',
         image: 'https://thumbs.dreamstime.com/z/perrito-lindo-de-la-mezclado-raza-del-golden-retriever-33418956.jpg',
     },
-
     {
         title: 'Doggo2',
         image: 'https://https://i.imgur.com/z9UAzcd.jpg.dreamstime.com/z/perrito-lindo-de-la-mezclado-raza-del-golden-retriever-33418956.jpg',
-    },
+    }
 ]
 
+let randomMovie = movies[Math.floor(Math.random()*movies.length)].title;
 
-button.addEventListener('click', function(){
-    image.style.backgroundImage = "url('https://thumbs.dreamstime.com/z/perrito-lindo-de-la-mezclado-raza-del-golden-retriever-33418956.jpg')";
-    image.style.backgroundSize = 'cover';
-    image.style.backgroundPosition = 'left';
+
+button.addEventListener('click', function () {
+    console.log(randomMovie);
+    // image.style.backgroundImage = "url('https://thumbs.dreamstime.com/z/perrito-lindo-de-la-mezclado-raza-del-golden-retriever-33418956.jpg')";
+    // image.style.backgroundSize = 'cover';
+    // image.style.backgroundPosition = 'left';
 
 })
 
